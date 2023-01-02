84c5ed05c93550494b142f2b05d4f6819fbf95ce
indra <indra.gunanda@dev.nusatek.id>
2023-01-02 15:27:30 +0700

    added : service getAnimeById

--- services/root/index.ts
+++ services/root/index.ts
@@ -1,6 +1,7 @@
 import { initializeHttp } from '../../configs/http.config';
 import { API_HOST } from '../../constants/http.constant';
 import { getAnimeSearchTypes } from '../../types/services/getAnimeSearchTypes';
+import { getAnimeByIdTypes } from '../../types/services/getAnimeById';
 
 const http = initializeHttp(API_HOST ?? '');
 /**
@@ -17,4 +18,18 @@ export const getAnimeSearch = (params: getAnimeSearchTypes.request) => {
     .catch((err) => {
       throw err;
     }) as Promise<getAnimeSearchTypes.response>;
+};
+
+/**
+ * @description Get anime by id
+ * @param params getAnimeByIdTypes.request
+ * @returns Promise<getAnimeByIdTypes.response>
+ */
+export const getAnimeById = (params: getAnimeByIdTypes.request) => {
+  return http
+    .get(`anime/${params.id}`)
+    .then((res) => res.data)
+    .catch((err) => {
+      throw err;
+    }) as Promise<getAnimeByIdTypes.response>;
 };
