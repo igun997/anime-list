7f36a8d9f6fa37b833df8745b909a4d0ced53c3f
indra <indra.gunanda@dev.nusatek.id>
2023-01-02 15:50:13 +0700

    added : back to list

--- pages/anime/[id].tsx
+++ pages/anime/[id].tsx
@@ -1,13 +1,25 @@
 import { LayoutConfigWithNextPage } from '../../configs/layout.config';
 import { useRouter } from 'next/router';
+import { Button, Col, Row } from 'antd';
+import { LeftOutlined } from '@ant-design/icons';
 
 const Index: LayoutConfigWithNextPage = () => {
-  const { id } = useRouter().query;
+  const { query, push } = useRouter();
+  const { id } = query;
+  const backToList = () => {
+    push('/');
+  };
   return (
-    <>
-      <div>{id}</div>
-    </>
+    <Row gutter={[10, 10]}>
+      <Col xs={24}>
+        <Button type={'link'} onClick={backToList} icon={<LeftOutlined />}>
+          Back to Anime List
+        </Button>
+      </Col>
+    </Row>
   );
 };
 
+Index.title = 'Detail Anime';
+Index.layout = 'base';
 export default Index;
