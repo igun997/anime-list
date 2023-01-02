2ae6f0b571cb107faf80e8abcfbf22a2ebabd469
indra <indra.gunanda@dev.nusatek.id>
2023-01-02 15:13:56 +0700

    added : current_page types

--- types/services/getAnimeSearchTypes.ts
+++ types/services/getAnimeSearchTypes.ts
@@ -89,6 +89,8 @@ export namespace getAnimeSearchTypes {
   export type pagination = {
     last_visible_page: number;
     has_next_page: boolean;
+    current_page: number;
+    
     items: {
       count: number;
       total: number;
